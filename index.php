d2871718fe56aec47c21f5993e79fc2db5484572
arthursiq5 <arthur.siq5@gmail.com>
2022-08-20 22:32:12 -0300

    Listados dados

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
+<!DOCTYPE html>
 <html>
-
 <head>
-<title>Exemplo PHP</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Exemplo PHP</title>
 </head>
 <body>
 
@@ -43,8 +46,21 @@ function processForm($connection, $data) {
   }
 }
 
+function getData($connection) {
+  $sql = 'SELECT id, nome, sobrenome, endereco, cidade FROM dados';
+  $result = $connection->query($sql);
+  $rows = [];
+  if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+      $rows[] = $row;
+    }
+  }
+  return $rows;
+}
+
+$connection = getConnection();
 if (!empty($_POST)) {
-  processForm(getConnection(), $_POST);
+  processForm($connection, $_POST);
 }
 
 
@@ -71,6 +87,29 @@ echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
     </div>
     <button style="box-shadow: 0 0 10px 5px green;" type="submit">Enviar</button>
   </form>
+
+  <table>
+    <thead>
+      <tr>
+        <th>id</th>
+        <th>nome</th>
+        <th>sobrenome</th>
+        <th>endere&ccedil;o</th>
+        <th>cidade</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach (getData($connection) as $row): ?>
+        <tr>
+          <td><?= $row['id'] ?></td>
+          <td><?= $row['nome'] ?></td>
+          <td><?= $row['sobrenome'] ?></td>
+          <td><?= $row['endereco'] ?></td>
+          <td><?= $row['cidade'] ?></td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
 
 </body>
 </html>
