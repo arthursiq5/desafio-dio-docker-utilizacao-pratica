e23a4ddc304275c0c9679dc42ae9b9e3030f7c04
arthursiq5 <arthur.siq5@gmail.com>
2022-08-20 21:32:02 -0300

    Criada base de dados

---
+++ banco.sql
@@ -0,0 +1,9 @@
+CREATE TABLE dados (
+    id INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(50),
+    sobrenome VARCHAR(50),
+    endereco VARCHAR(150),
+    cidade VARCHAR(50),
+    host VARCHAR(50),
+    primary key(id)
+);
